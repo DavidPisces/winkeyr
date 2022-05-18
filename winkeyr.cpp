17da2f388af8aa0fd6d62dd1d5b5f149d0a40005
Lingluo <Lingluo@DESKTOP-O3GM1CM>
2022-05-19 00:36:02 +0800

    添加项目文件。

---
+++ winkeyr.cpp
@@ -0,0 +1,29 @@
+﻿#include <Windows.h>
+#include <WinUser.h>
+/* 
+    WinKey demo for Mydockfinder
+    Open Start menu
+    Made by Lingluo
+    Follow GNU General Public License v3.0
+*/
+#pragma comment( linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"" )
+
+int main()
+{
+    INPUT inputs[4] = {};
+    Sleep(100);
+    inputs[0].type = INPUT_KEYBOARD;
+    inputs[0].ki.wVk = VK_CONTROL;
+    inputs[2].type = INPUT_KEYBOARD;
+    inputs[2].ki.wVk = VK_CONTROL;
+    inputs[2].ki.dwFlags = KEYEVENTF_KEYUP;
+
+    inputs[1].type = INPUT_KEYBOARD;
+    inputs[1].ki.wVk = VK_ESCAPE;
+    inputs[3].type = INPUT_KEYBOARD;
+    inputs[3].ki.wVk = VK_ESCAPE;
+    inputs[3].ki.dwFlags = KEYEVENTF_KEYUP;
+    SendInput(ARRAYSIZE(inputs), inputs, sizeof(INPUT));
+    exit(0);
+    return 0;
+}
